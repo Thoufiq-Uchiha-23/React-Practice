4b7870c60fcd991a9eb49d1b0be6675982f57051
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-05 15:25:31 +0530

    App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello from App</h1>
+            <Todo />
+        </div>
+    )
+}
+
+export default App;
