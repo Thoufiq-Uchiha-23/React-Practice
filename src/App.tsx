fd4d12be5a9c8bdba256e296e80586f9ed3781a2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-06 22:46:07 +0530

    app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Todo from "./components/Todo";
 
+const myItems = [
+    {
+        id: 1,
+        title: "I need to finish my hw"
+    },
+    {
+        id: 2,
+        title: "I need to watch the new episode of my favorite show"
+    }
+]
+
 const App: React.FC = () => {
     return (
         <div>
             <h1>Hello from App</h1>
-            <Todo />
+            <Todo items={myItems} />
         </div>
     )
 }
