4021598b9406fb81a21dfc0cb5c8952b5ac5f762
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-06 22:46:21 +0530

    Todo component with props

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,12 +1,31 @@
 import TodoItem from "./TodoItem"
 
-const Todo: React.FC = () => {
+interface ITodoItem {
+    id: number,
+    title: string,
+}
+
+interface TodoProps {
+    items: ITodoItem[]
+}
+
+const Todo: React.FC<TodoProps> = (props) => {
     return (
         <div>
             <ol>
-                <TodoItem />
-                <TodoItem />
-                <TodoItem />
+                {props.items.map(item => <TodoItem key={item.id} title={item.title} />)}
+            </ol>
+            <ol>
+                <TodoItem title="Eat"/>
+                <TodoItem title="Code"/>
+                <TodoItem title="Sleep"/>
+                <TodoItem title="Repeat"/>
+            </ol>
+            <ol>
+                <TodoItem title="Eat"/>
+                <TodoItem title="Code"/>
+                <TodoItem title="Sleep"/>
+                <TodoItem title="Repeat"/>
             </ol>
         </div>
     )
