30b1522982926976fb3cfa8b0d5501da181f49eb
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-15 11:37:26 +0530

    App updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
 import Todo from "./components/Todo";
+import Counter from "./components/Counter";
 
 const myItems = [
-    {
-        id: 1,
-        title: "I need to finish my hw"
-    },
-    {
-        id: 2,
-        title: "I need to watch the new episode of my favorite show"
-    }
-]
+  {
+    id: 1,
+    title: "I need to finish my hw",
+  },
+  {
+    id: 2,
+    title: "I need to watch the new episode of my favorite show",
+  },
+];
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <h1>Hello from App</h1>
-            <Todo items={myItems} />
-        </div>
-    )
-}
+  const [state, setState] = useState<boolean>(true);
+
+  return (
+    <div>
+      <h1>Hello from App</h1>
+      <button onClick={(e) => setState(!state)}>Toggle</button>
+      {state ? <Counter /> : ""}
+      <Todo items={myItems} />
+    </div>
+  );
+};
 
 export default App;
